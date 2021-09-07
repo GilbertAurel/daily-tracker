043d4727a7698062a891bba23a2e90944f990a26
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-09-07 12:52:42 +0700

    feat: add margin padding for heading

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -6,6 +6,7 @@ export const COLORS = {
 
   white: '#ffffff',
   gray: '#878787',
+  lightgray: '#EFEFEF',
   black: '#353642',
 
   notification: '#FF5252',
@@ -15,30 +16,32 @@ export const COLORS = {
 
 export const FONTS = {
   h1: css`
+    margin: 0;
+    padding: 0;
     font-size: 2.5rem;
-    font-weight: 700;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `,
   h2: css`
+    margin: 0;
+    padding: 0;
     font-size: 1.5rem;
-    font-weight: 700;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `,
   subtitle: css`
     font-size: 1rem;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `,
   body: css`
     font-size: 0.75rem;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `,
   button: css`
     font-size: 0.5rem;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `,
   preTitle: css`
     font-size: 0.5rem;
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   `
 };
 
