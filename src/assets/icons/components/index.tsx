73745d2a6e47ff2076b9ae03bff802b16457fe95
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-09-09 10:45:26 +0700

    feat: add new icon

--- src/assets/icons/components/index.tsx
+++ src/assets/icons/components/index.tsx
@@ -2,4 +2,5 @@ export { default as Bell } from './Bell';
 export { default as Globe } from './Globe';
 export { default as Home } from './Home';
 export { default as Layers } from './Layers';
+export { default as Plus } from './Plus';
 export { default as User } from './User';
