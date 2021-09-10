49184d7e4a2799ed475440438a192b83ea9493d7
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-09-10 22:48:06 +0700

    feat: add sub menu on plus button

--- src/components/home-add-button/__test__/index.test.tsx
+++ src/components/home-add-button/__test__/index.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
 import HomeAddButton from '../index';
 
 it('should render add button', () => {
@@ -6,4 +6,12 @@ it('should render add button', () => {
 
   const addButton = screen.getByTestId('add-btn');
   fireEvent.click(addButton);
+});
+
+it('should show sub menu after button click', async () => {
+  render(<HomeAddButton />);
+
+  const addButton = screen.getByTestId('add-btn');
+  waitFor(() => fireEvent.click(addButton));
+  expect(screen.queryAllByTestId('sub-menu')).toHaveLength(1);
 });
--- src/components/home-add-button/index.tsx
+++ src/components/home-add-button/index.tsx
@@ -1,36 +1,91 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import { Plus } from 'assets/icons/components';
-import { COLORS, SIZES } from 'assets/theme';
-import React from 'react';
+import React, { useState } from 'react';
+import { Layers, Plus } from 'assets/icons/components';
+import { COLORS, FONTS, SIZES } from 'assets/theme';
 
 const HomeAddButton: React.FC = () => {
+  const [subMenu, setSubMenu] = useState(false);
+
+  const toggleSubMenu = () => setSubMenu(!subMenu);
+
   const styles = {
-    button: css`
-      height: 3rem;
-      width: 3rem;
+    container: css`
       position: absolute;
       right: 1.5rem;
       bottom: 8rem;
       display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 0.25rem;
+    `,
+    button: css`
+      height: 3rem;
+      width: 3rem;
+      display: flex;
       justify-content: center;
       align-items: center;
+      background-color: ${subMenu ? COLORS.secondary : COLORS.primary};
+      border: none;
+      border-radius: 0.75rem;
+
+      :active {
+        transform: scale(0.9);
+      }
+    `,
+    subButton: css`
+      padding: 0.75rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.25rem;
       background-color: ${COLORS.primary};
       border: none;
       border-radius: 0.75rem;
+
+      p {
+        margin: 0;
+        padding: 0;
+        color: ${COLORS.white};
+        ${FONTS.body}
+      }
+
+      :active {
+        transform: scale(0.9);
+      }
     `,
-    icon: {
-      height: SIZES.icon,
-      width: SIZES.icon,
-      color: COLORS.white
-    }
+    icon: css`
+      height: ${SIZES.icon};
+      width: ${SIZES.icon};
+      color: ${subMenu ? COLORS.primary : COLORS.white};
+      transform: ${subMenu ? 'rotate(45deg)' : 'rotate(0deg)'};
+      transition: transform 0.3s ease;
+    `,
+    subIcon: css`
+      height: ${SIZES.iconSmall};
+      width: ${SIZES.iconSmall};
+      color: ${COLORS.white};
+    `
   };
 
   return (
-    <button css={styles.button} type="button" data-testid="add-btn">
-      <Plus {...styles.icon} />
-    </button>
+    <div css={styles.container}>
+      {subMenu && (
+        <button css={styles.subButton} type="button" data-testid="sub-menu">
+          <Layers css={styles.subIcon} />
+          <p>create new</p>
+        </button>
+      )}
+      <button
+        css={styles.button}
+        type="button"
+        onClick={toggleSubMenu}
+        data-testid="add-btn"
+      >
+        <Plus css={styles.icon} />
+      </button>
+    </div>
   );
 };
 
